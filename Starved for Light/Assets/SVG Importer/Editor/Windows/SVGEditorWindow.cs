90cc77482c4c7fe92d3dc75afdad1a7548472140
MISIEK <bezi049pl@gmail.com>
2017-08-23 23:40:34 +0200

    SVG Importer update

--- Starved for Light/Assets/SVG Importer/Editor/Windows/SVGEditorWindow.cs
+++ Starved for Light/Assets/SVG Importer/Editor/Windows/SVGEditorWindow.cs
@@ -933,10 +933,10 @@ namespace SVGImporter
 			Bounds bounds = mesh.bounds;
 			float magnitude = bounds.extents.magnitude;
 			float num = 4f * magnitude;
-			previewUtility.camera.transform.position = -Vector3.forward * num;
-			previewUtility.camera.transform.rotation = Quaternion.identity;
-			previewUtility.camera.nearClipPlane = num - magnitude * 1.1f;
-			previewUtility.camera.farClipPlane = num + magnitude * 1.1f;
+			previewUtility.m_Camera.transform.position = -Vector3.forward * num;
+			previewUtility.m_Camera.transform.rotation = Quaternion.identity;
+			previewUtility.m_Camera.nearClipPlane = num - magnitude * 1.1f;
+			previewUtility.m_Camera.farClipPlane = num + magnitude * 1.1f;
 			
 			Quaternion quaternion = Quaternion.identity;
 			Vector3 pos = -bounds.center;
@@ -946,7 +946,7 @@ namespace SVGImporter
 			int meshSubset = materials.Length;
 			if (materials != null && materials.Length > 0)
 			{
-				previewUtility.camera.clearFlags = CameraClearFlags.Nothing;
+				previewUtility.m_Camera.clearFlags = CameraClearFlags.Nothing;
 				if (meshSubset < 0 || meshSubset >= subMeshCount)
 				{
 					for (int i = 0; i < subMeshCount; i++)
@@ -958,7 +958,7 @@ namespace SVGImporter
 				{
 					previewUtility.DrawMesh(mesh, pos, quaternion, materials[0], -1);
 				}
-				previewUtility.camera.Render();
+				previewUtility.m_Camera.Render();
 			}
 			Unsupported.SetRenderSettingsUseFogNoDirty(fog);
 		}
