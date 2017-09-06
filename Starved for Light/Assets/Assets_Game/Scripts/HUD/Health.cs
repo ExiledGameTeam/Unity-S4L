05e093ac7c877ae68e7aed8933710a6ea453bc0b
swroblewski <sylwek.wroblewski@gmail.com>
2017-09-06 23:37:11 +0200

    Hud positioning and health implementation

---
+++ Starved for Light/Assets/Assets_Game/Scripts/HUD/Health.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+	public float health = 100f;
+	public float maxHealth = 100f;
+	public float waitTime = 5f;
+	public float regenerationTime = 1000f;
+
+	public float lastTimeHit;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		lastTimeHit += Time.deltaTime;
+		if(lastTimeHit > waitTime){
+			health += 1.0f/regenerationTime * maxHealth;
+			NormalizeHealth();
+		}
+	}
+
+	public void GetHit(float damage){
+		lastTimeHit = 0f;
+		health -= damage;
+		NormalizeHealth();
+	}
+
+	private void NormalizeHealth(){
+		if(health < 0){
+			health = 0;
+		} else if (health > maxHealth) {
+			health = maxHealth;
+		}
+	}
+
+	public void OnMouseDown(){
+		GetHit(90);
+	}
+}
