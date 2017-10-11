ba966d4aee85dd16123cc9d2cd246c8c79074ec8
Markus (Bleson) Hyvärinen <blesonfication@gmail.com>
2017-10-11 21:44:41 +0300

    - Added Iris script, which handles inputs to other its components - WIP experimental movement testing - Reseted physics settings

---
+++ Starved for Light/Assets/Starved for Light/Scripts/Components/Iris.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace S4L {
+    [RequireComponent(typeof(MovementComponent))]
+    public class Iris : MonoBehaviour {
+
+        private MovementComponent movement;
+
+        private void Awake() {
+            movement = GetComponent<MovementComponent>();
+        }
+
+        private void Start() {
+            EventManager.Trigger(EventManager.OnCameraTargetChanged, transform);
+        }
+
+        private void OnEnable() {
+            EventManager.OnPlayerInput += HandleInputs;
+        }
+
+        private void OnDisable() {
+            EventManager.OnPlayerInput -= HandleInputs;
+        }
+
+        void HandleInputs(
+            List<Tuple<Enums.PlayerAxisInput, float>> inputAxises,
+            List<Tuple<Enums.PlayerButtonInput, Enums.ButtonEvent>> inputButtons
+        ) {
+            foreach (var pair in inputAxises) {
+                if (pair.value1 == Enums.PlayerAxisInput.Horizontal) {
+                    movement.MoveHorizontal(pair.value2);
+                    print("Received input!");
+                }
+            }
+        }
+    }
+}
---
+++ Starved for Light/Assets/Starved for Light/Scripts/Components/MovementComponent.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace S4L {
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class MovementComponent : MonoBehaviour {
+
+        private Rigidbody2D rb;
+        private Transform t;
+
+        public bool asd;
+        public float maxWalkSpeed = 1f;
+
+        private void Awake() {
+            rb = GetComponent<Rigidbody2D>();
+            t = GetComponent<Transform>();
+        }
+
+        public void MoveHorizontal(float direction) {
+            if (asd) {
+                rb.MovePosition((Vector2)t.position +
+                    Time.deltaTime * new Vector2(
+                    direction * maxWalkSpeed, 0f) +
+                    rb.velocity +
+                    rb.gravityScale * Physics2D.gravity * Time.deltaTime
+                );
+            } else {
+                rb.AddForce(new Vector2(1000f * Time.deltaTime * direction, 0f));
+                rb.velocity = Vector2.ClampMagnitude(rb.velocity, maxWalkSpeed);
+            }
+        }
+    }
+}
