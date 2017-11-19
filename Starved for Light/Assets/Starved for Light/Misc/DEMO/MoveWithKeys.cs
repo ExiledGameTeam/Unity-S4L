959cb8534506e4cdce64298abae983ba9fd1754f
MISIEK <bezi049pl@gmail.com>
2017-11-19 22:47:22 +0100

    Project fix
    Gitignore was wrongly written. Cause of that, some of the files weren't uploaded to Github.
    
    This contains all needed files and a rewritten .gitignore file.
    
    It *should* fix it. (but might not)

---
+++ Starved for Light/Assets/Starved for Light/Misc/DEMO/MoveWithKeys.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TeamUtility.IO;
+
+public class MoveWithKeys : MonoBehaviour {
+
+  public float mSpeed;
+
+	void Start () {
+    }
+	
+
+	void Update () {
+        transform.Translate(mSpeed * InputManager.GetAxis("Horizontal") * Time.deltaTime, mSpeed * InputManager.GetAxis("Vertical") * Time.deltaTime, 0f);
+	}
+}
